3db5c5d5e3acc3739520863479e7c644286cac1e
sebacaccaro <sebastianocaccaro@gmail.com>
2018-10-24 01:55:29 +0200

    Bozza di catalogo. isolato php connect.

--- php/phpConnect.php
+++ php/phpConnect.php
@@ -1,22 +1,3 @@
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-	<title>LumBooks</title>
-	<link rel="icon" href=""/> <!-- link icona -->
-	<meta name="title" content="LumBooks" />
-	<meta name="description" content="Acquista e vendi libri" /> <!-- da fare -->
-	<meta name="keywords" content="libro, unipd" /> <!-- da fare -->
-	<meta name="language" content="italian it" />
-	<meta name="author" content="" /> <!-- da fare -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="../css/style.css" /> <!-- da fare -->
-  <link rel="stylesheet" type="text/css"  href="../css/" media="screen and (max-width:768px)" /> <!-- schermi piccoli --> <!-- da fare -->
-  <link rel="stylesheet" type="text/css" href="../css/" media="print" /> <!-- da fare -->
-</head>
-<body>
-
-<h3>Welcome to the PHP Connect Test</h3>
-
 <?php
 $dbname = 'LB';
 $dbuser = 'admin';
@@ -24,9 +5,10 @@ $dbpass = 'admin';
 $dbhost = 'localhost';
 $connect = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if($connect->connect_errno){
-	echo "lol";
+	echo "Conncection Error";
 	exit();
 }
+mysqli_set_charset($connect,"utf8");
 if(!$result = $connect->query("SELECT * FROM Libri_Listati")){
 	echo "Errore di query";
 	exit();
@@ -34,16 +16,6 @@ if(!$result = $connect->query("SELECT * FROM Libri_Listati")){
 else{
 	$connect->close();
 }
-if($result->num_rows > 0){
-	while($row = $result->fetch_array(MYSQLI_ASSOC)){
-		$str = "<p>".$row['Titolo']." | ".$row['Autore']." | ".$row['Casa_Editrice']." | ".$row['Corso']."</p>";
-		$str = mb_convert_encoding($str, "utf-8", "auto");
-		echo $str;
-	}
-	$result->free();
-}
 
 ?>
 
-</body>
-</html>
