a5bcfb7f82a645391d2712f78d151bd119c83408
Stefano Zanatta <stefano.sgt39@gmail.com>
2018-10-26 22:28:07 +0200

    ugi

--- php/Backend/back_end.php
+++ php/Backend/back_end.php
@@ -3,42 +3,42 @@ class back_end{
     //Controlla l'hash della password inserita con l'hash nel database.
     //Se corrispondono, ritorno 1, 0 altrimenti.
     public static function checkPassword($email, $password){
-        
+
         //Controllo validità variabili
         if(!(isset($email) and isset($password))){
             return array(
 				"password_ok" => false,
-				"error" => "Richiesta incompleta",
-			}
+				"error" => "Richiesta incompleta"
+			);
         }
-        
+
         //includo la connessione al database
         include "phpConnect.php";
-        
+
         if(!$result = $connect->query("SELECT * FROM Utente WHERE Email = '$email'")){
             //errore di query
             return array(
 				"password_ok" => false,
-				"error" => "Errore di query",
-			}
+				"error" => "Errore di query"
+			);
 		}
         
         $connect->close();
-        
+
 		if($result->num_rows > 0){
 			$row = $result->fetch_array(MYSQLI_ASSOC);
 			$dbhash = $row["Pw_Hash"];
             if (password_verify($password, $dbhash)) {
                 return array(
                     "password_ok" => true,
-                    "error" => "",
-                }
-            } 
+                    "error" => ""
+                );
+            }
         }
         return array(
             "password_ok" => false,
-            "error" => "password e/o mail sbagliata",
-        };
+            "error" => "password e/o mail sbagliata"
+        );
     }
 
 }
