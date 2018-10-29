f2b67bb8d2b1faa7645087b9a3ce7e60393511d4
Stefano Zanatta <stefano.sgt39@gmail.com>
2018-10-29 10:02:08 +0100

    rimosse dipendenze esterne

--- js/buildStructure.js
+++ js/buildStructure.js
@@ -1,6 +1,6 @@
 $( document ).ready(function()
 {
   $("header").load('../HTML/header.html');
-  $("footer").load('../HTML/footer.html');
   $("nav").load('../HTML/navbar.html');
+  $("footer").load('../HTML/footer.html');
 });
