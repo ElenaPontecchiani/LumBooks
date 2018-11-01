b2c1c0f1a99363023216809b16c7c7df54bc854a
Stefano Zanatta <stefano.sgt39@gmail.com>
2018-11-01 13:41:16 +0100

    da completare la ricerca libri (stampa html- per stefano)

--- js/searchBook.js
+++ js/searchBook.js
@@ -1,6 +1,29 @@
 
+
+
 $( document ).ready(function()
 {
+  /*
+    riempie il select #titleInput con i titoli
+  */
+  var t;
+  $.post("../php/Backend/controller.php",
+  {
+    command: 'getTitles'
+  }, function(res)
+  {
+    t = JSON.parse(res).titoli;
+    titleshtml = "<select id='titleInput' class='formText'>";
+    for(i in t){
+      titleshtml += "<option value='"+ t[i] +"'>"+ t[i] +"</option>";
+    }
+    titleshtml += "</select>";
+    $('#titleInput').html(titleshtml);
+  });
+
+  /*
+    cerca i libri
+  */
   $('#submitSrc').click(function()
     {
       var titolo = $('#titleInput').val();
@@ -12,55 +35,37 @@ $( document ).ready(function()
       // creare la pagina con i libri trovati - (html deve essere completo) (!!!)
     });
 
-
+    /*
+      funzione: cercaLibro
+      ricerca i libri dati dei parametri
+      return: array di libri. libri è un array della struttura {titolo,autore,prezzo,isbn}
+    */
+    function cercaLibro(titolo, autore, isbn, corso, ordine)
+    {
+                                        //inserire le funzioni nel ready? (!!!)
+      $.post("../php/Backend/controller.php",
+      {
+        command: 'searchBook',
+        titolo: titolo,
+        autore: autore,
+        isbn: isbn,
+        corso: corso,
+        ordine: ordine
+      }, function(res)
+      {
+        var obj = JSON.parse(res);
+        if(obj.error == ""){
+          for(var i in obj){
+              libri[i].titolo = obj.titolo[i];
+              libri[i].autore = obj.autore[i];
+              libri[i].prezzo = obj.prezzo[i];
+              libri[i].isbn = obj.isbn[i];
+          }
+          return libri;
+        }else {
+          return obj.error;
+        }
+      });
+    }
 
 });
-
-/*
-  funzione: cercaLibro
-  ricerca i libri dati dei parametri
-  return: array di libri. libri è un array della struttura {titolo,autore,prezzo,isbn}
-*/
-function cercaLibro(titolo, autore, isbn, corso, ordine)
-{                                     //inserire le funzioni nel ready? (!!!)
-  $.post("../php/Backend/controller.php",
-  {
-    command: 'searchBook',
-    titolo: titolo,
-    autore: autore,
-    isbn: isbn,
-    corso: corso,
-    ordine: ordine
-  }, function(res)
-  {
-    alert(res);  //errore nella traduzione json ->obj (!!!)
-    var obj = JSON.parse(res);
-    if(obj.error == ""){
-      for(var i in obj){
-          libro[i].titolo = obj.titolo[i];
-          libro[i].autore = obj.autore[i];
-          libro[i].prezzo = obj.prezzo[i];
-          libro[i].isbn = obj.isbn[i];
-      }
-      return libro;
-    }else {
-      return obj.error;
-    }
-  });
-}
-
-/*
-  funzione: getTitles
-  ritorna la lista dei titolo dei libri in vendita
-*/
-function titoli()
-{
-  $.post("../php/Backend/controller.php",
-  {
-    command: 'getTitles'
-  }, function(res)
-  {
-    alert(res);
-    var obj = JSON.parse(res);
-  });
-}
