5e3a777f7115066d7a24b16efab871d1de9acb31
sebacaccaro <sebastianocaccaro@gmail.com>
2018-11-06 15:51:30 +0100

    Cominciata conversione a php. Fatto home.php come esempio. Create cartelle per il sorting dei file.

---
+++ php/home.php
@@ -0,0 +1,24 @@
+<?php
+echo "<!DOCTYPE html>";
+echo '<html lang="it">';
+
+//HEAD
+include "../HTML/head/home.html";
+
+
+
+echo "<body>";
+
+
+    include "../HTML/modules/header.html";
+    include "../HTML/modules/navbar.html";
+    
+    include "../HTML/body/home.html";
+
+    include "../HTML/modules/footer.html";
+
+echo "</body>";
+
+
+echo "</html>";
+?>
