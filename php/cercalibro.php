8c196db47a20980de6389019009564b7e9858f77
sebacaccaro <sebastianocaccaro@gmail.com>
2018-11-06 16:43:30 +0100

    Parte pagine

---
+++ php/cercalibro.php
@@ -0,0 +1,24 @@
+<?php
+echo "<!DOCTYPE html>";
+echo '<html lang="it">';
+
+//HEAD
+include "../HTML/head/cercalibro.html";
+
+
+
+echo "<body>";
+
+
+    include "../HTML/modules/header.html";
+    include "../HTML/modules/navbar.html";
+    
+    include "../HTML/body/cercalibro.html";
+
+    include "../HTML/modules/footer.html";
+
+echo "</body>";
+
+
+echo "</html>";
+?>
