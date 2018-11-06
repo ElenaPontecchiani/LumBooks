52162c0e09bcdaac8fc476b62dc157e1ab7c6fa4
Stefano Zanatta <stefano.sgt39@gmail.com>
2018-11-06 16:35:33 +0100

    conversione php - registrati

---
+++ php/registrati.php
@@ -0,0 +1,24 @@
+<?php
+echo "<!DOCTYPE html>";
+echo '<html lang="it">';
+
+//HEAD
+include "../HTML/head/registrati.html";
+
+
+
+echo "<body>";
+
+
+    include "../HTML/modules/header.html";
+    include "../HTML/modules/navbar.html";
+
+    include "../HTML/body/registrati.html";
+
+    include "../HTML/modules/footer.html";
+
+echo "</body>";
+
+
+echo "</html>";
+?>
