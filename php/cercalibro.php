541a31386fed99471498b9e31e01497861434572
Sebastiano <sebastianocaccaro@gmail.com>
2018-11-11 15:55:38 +0100

    Passato cercalibro al nuovo formato

--- php/cercalibro.php
+++ php/cercalibro.php
@@ -1,51 +1,38 @@
 <?php
 //importazione della classe sql wrapper
 require_once("Backend/sql_wrapper.php");
-
-echo "<!DOCTYPE html>";
-echo '<html lang="it">';
-
-//HEAD
-include "../HTML/head/cercalibro.html";
-
-
-
-echo "<body>";
+require_once("Backend/htmlMaker.php");
     
-    //lista corsi e titoli sono array di stringhe
-    $lista_corsi = SqlWrap::query("SELECT distinct Corso FROM Libri_Listati",true);
-    $lista_titoli = SqlWrap::query("SELECT distinct Titolo FROM Libri_Listati",true);
+//lista corsi e titoli sono array di stringhe
+$lista_corsi = SqlWrap::query("SELECT distinct Corso FROM Libri_Listati",true);
+$lista_titoli = SqlWrap::query("SELECT distinct Titolo FROM Libri_Listati",true);
 
-    /*creazione html da inserire*/
-    //corsi
-    $opzioni_corsi = "";    
-    if ($lista_corsi){
-        foreach($lista_corsi as $corso){
-            $opzioni_corsi .= "<option value=\"$corso\">$corso</option>\n";
-        }
+/*creazione html da inserire*/
+//corsi
+$opzioni_corsi = "";    
+if ($lista_corsi){
+    foreach($lista_corsi as $corso){
+        $opzioni_corsi .= "<option value=\"$corso\">$corso</option>\n";
     }
-    //titoli
-    $opzioni_titolo = "";    
-    if ($lista_titoli){
-        foreach($lista_titoli as $titolo){
-            $opzioni_titolo .= "<option value=\"$titolo\">\n";
-        }
+}
+//titoli
+$opzioni_titolo = "";    
+if ($lista_titoli){
+    foreach($lista_titoli as $titolo){
+        $opzioni_titolo .= "<option value=\"$titolo\">\n";
     }
-    
-    //inserimento opzioni corsi nella pagina
-    $html = file_get_contents("../HTML/body/cercalibro.html");
-    $html = str_replace("££opzioni££",$opzioni_corsi,$html);
-    $html = str_replace("££titoli££",$opzioni_titolo,$html);  
+}
 
-    include "../HTML/modules/header.html";
-    include "../php/modules/navbar.php";
-    
-    //stampa del body principale della pagina
-    echo $html;
-
-    include "../HTML/modules/footer.html";
-echo "</body>";
+//inserimento opzioni corsi nella pagina
+$output = file_get_contents("../HTML/cercalibro.html");
+$output = str_replace("££opzioni££",$opzioni_corsi,$output);
+$output = str_replace("££titoli££",$opzioni_titolo,$output);
+$output = str_replace("<header></header>",htmlMaker::header(),$output);
+$output = str_replace("<nav></nav>",      htmlMaker::navbar(),$output);  
+$output = str_replace("<footer></footer>",htmlMaker::footer(),$output);  
 
 
-echo "</html>";
+//stampa del body principale della pagina
+echo $output;
+
 ?>
