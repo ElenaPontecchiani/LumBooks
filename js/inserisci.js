1d3e5b03b770a0eb87372765d0721dd1fa077b1a
Stefano Zanatta <stefano.sgt39@gmail.com>
2018-11-13 17:43:04 +0100

    js inserimento libro implementato

--- js/inserisci.js
+++ js/inserisci.js
@@ -1,15 +1,19 @@
 window.onload=function(){
-  radio();
-  document.getElementById("listato").addEventListener("click", radio);
-  document.getElementById("personale").addEventListener("click", radio);
+  formselector();
+  document.getElementById("listato").addEventListener("click", formselector);
+  document.getElementById("personale").addEventListener("click", formselector);
 }
 
 // sto pensando a un nome decente per il metodo. lo finisco quando ho l'html
-function radio(){
-  listato = document.getElementById('listato').selected;
-  if(listato)
-    alert("nascondi titolo, autore, casaeditrice, corso");
-  else
-    alert("nascondi select catalogo");
+function formselector(){
+  listato = document.getElementById('listato').checked;
+  if(listato){
+    document.getElementById('inserisci-personale').classList.add("hidden");
+    document.getElementById('inserisci-listato').classList.remove("hidden");
+  }
+  else{
+    document.getElementById('inserisci-listato').classList.add("hidden");
+    document.getElementById('inserisci-personale').classList.remove("hidden");
+  }
 
 }
