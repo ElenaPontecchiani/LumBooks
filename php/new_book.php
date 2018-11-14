4b7f7f313b0e113f0b6cc58b83e56a3bd737089d
Sebastiano <sebastianocaccaro@gmail.com>
2018-11-14 12:44:51 +0100

    Inizio modularizzazione registrazione valori

--- php/new_book.php
+++ php/new_book.php
@@ -28,49 +28,12 @@ $isbn =             $_POST['ISBN'];
 $prezzo =           $_POST['prezzo'];
 $libro_catalogo =   $_POST['catalogo'];
 
-//Escape di tutti gli input
-//Tutti insieme per minimiazzare accesso al db
-SqlWrap::input_escape(array(  &$scelta_list,&$titolo,&$autore,&$casa_editrice,
-                        &$corso,&$edizione,&$annopub,&$isbn,
-                        &$prezzo,&$libro_catalogo));
 
-//A seconda della scelta dell'utente (listato vs libero), controllo solo
-//i campi necessari
-if ($scelta_list == "listato"){
-    $scelta_list = true;
-    if(!Validator::lengthVal(3,50,$libro_catalogo))
-        throw new Exception("Lunghezza libro da catalogo errata");
-}
-else{
-    $scelta_list = false;
-    if(!Validator::lengthVal(3,50,$titolo,$autore,$corso,$casa_editrice))
-        throw new Exception("Lunghezze sbagliate o campi vuoti");
-  
-}
-
-//Annopub può essere null oppure un anno valido
-if ($annopub != "" &&
-    (!filter_var($annopub, FILTER_VALIDATE_INT, array("options" => array("min_range"=>1900, "max_range"=>2100)))))
-    throw new Exception("Anno non valido");
-
-//Saranno necessari controlli per stabilire che valore di ezione sia corretto
-#############################################################################
-if((!Validator::lengthVal(3,20,$edizione)) && $edizione != "")
-    throw new Exception("Edizione non valida");
-
-//Validazione anno di pubblicazione
-if  (( (!(filter_var($annopub, FILTER_VALIDATE_INT))) || strlen($isbn) != 13) && $annopub != "")
-    throw new Exception("ISBN non valido");
-
-//Validazione del prezzo
-$prezzo = str_replace(",",".",$prezzo);
-$pattern = '/^\d+(?:\.\d{2})?$/';
-if (!preg_match($pattern, $prezzo)) {
-    throw new Exception("Prezzo non valido");
- }
+Validator::registerVal( $scelta_list, $titolo, $autore, $casa_editrice,
+                        $corso, $edizione, $annopub, $isbn, $prezzo, $libro_catalogo); 
     
                         
-
+//MANCA TRIM DEGLI INPUT
 
 
 
