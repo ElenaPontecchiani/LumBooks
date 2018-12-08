15a1b25c2c9dd4ad5172b473d3e1ca042e8121f9
stefano <stefano.sgt39@gmail.com>
2018-12-08 17:59:54 +0100

    small fix-2

--- php/catalogo.php
+++ php/catalogo.php
@@ -2,13 +2,13 @@
 require_once "Backend/htmlMaker.php";
 require_once "Backend/sql_wrapper.php";
 
-$query = "  SELECT Titolo,Autore,Casa_Editrice as Editore, Corso 
+$query = "  SELECT Titolo,Autore,Casa_Editrice as Editore, Corso
             FROM Libri_Listati";
 $libri = htmlMaker::searchItemCatalog(SqlWrap::query($query));
 $output = file_get_contents("../HTML/catalogo.html");
 $output = str_replace("<dl class=\"search_item\"></dl>",$libri,$output);
 $output = str_replace("<header></header>",htmlMaker::header(),$output);
-$output = str_replace("<nav></nav>",      htmlMaker::navbar(),$output);  
+$output = str_replace("<nav></nav>",      htmlMaker::navbar(),$output);
 $output = str_replace("<footer></footer>",htmlMaker::footer(),$output); 
 
 echo $output;
--- php/home.php
+++ php/home.php
@@ -2,7 +2,9 @@
 require_once "Backend/htmlMaker.php";
 $output = file_get_contents("../HTML/home.html");
 echo str_replace("<nav></nav>",htmlMaker::navbar(),
-     str_replace("<header></header>",htmlMaker::header(),
-     str_replace("<footer></footer>",htmlMaker::footer(),
-                 $output)));
+      str_replace('<a href="Home.php">','',
+      str_replace("</h1></a>","</h1>",
+      str_replace("<header></header>",htmlMaker::header(),
+      str_replace("<footer></footer>",htmlMaker::footer(),
+                 $output)))));
 ?>
