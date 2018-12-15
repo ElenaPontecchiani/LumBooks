f3bd8aad52caff47658f23bcc8ca4d642f356094
stefano <stefano.sgt39@gmail.com>
2018-12-15 11:28:44 +0100

    about.php aggiunta al footer. css per about.html

---
+++ php/about.php
@@ -0,0 +1,10 @@
+<?php
+require_once "Backend/htmlMaker.php";
+$output = file_get_contents("../HTML/about.html");
+echo str_replace("<nav></nav>",htmlMaker::navbar(),
+      str_replace('<a href="Home.php">','',
+      str_replace("</h1></a>","</h1>",
+      str_replace("<header></header>",htmlMaker::header(),
+      str_replace("<footer></footer>",htmlMaker::footer(),
+                 $output)))));
+?>
