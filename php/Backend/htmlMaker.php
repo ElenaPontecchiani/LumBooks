939ceca8de56443a19d324782d3c2f1e32fb8710
stefano <stefano.sgt39@gmail.com>
2018-12-19 07:08:02 +0100

    rip

--- php/Backend/htmlMaker.php
+++ php/Backend/htmlMaker.php
@@ -123,10 +123,6 @@ class htmlMaker{
           $nav_return .=  '<li class=""><a href="inserisci.php">Inserisci</a></li>'."\n";
         }
         $nav_return .=  '</ul>'."\n";
-        $nav_return .=  '<form id="search-bar" action="risultati_ricerca.php" method="get">'."\n";
-        $nav_return .=  '<input type="text" name="titolo" placeholder="Cerca.."/>';
-        $nav_return .=  '<input type="submit" value="cerca"/>'."\n";
-        $nav_return .=  '</form>'."\n";
         $nav_return .=  '</nav>'."\n";
 
         return $nav_return;
