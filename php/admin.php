67aa58bb1e58eca166f7d19ca4d7404a119476bb
stefano <stefano.sgt39@gmail.com>
2019-02-05 23:33:46 +0100

    completato strumenti (parte principale). manca css

--- php/admin.php
+++ php/admin.php
@@ -3,7 +3,7 @@ require_once "Backend/htmlMaker.php";
 require_once "Backend/sql_wrapper.php";
 if(sqlWrap::isAdmin()){
       $output = file_get_contents("../HTML/admin.html");
-      $libri = htmlMaker::searchItemWithButtons(SqlWrap::query("SELECT * FROM Libri_Listati;"),array("Elimina"));
+      $libri = htmlMaker::generateBookCollection(SqlWrap::query("SELECT * FROM Libri_Listati;"),array("Elimina"));
       echo str_replace("<nav></nav>",htmlMaker::navbar(),
             str_replace('<dl class="search_item"></dl>',$libri,
             str_replace("<header></header>",htmlMaker::header(),
