665bcdb9e39df8010bb90c7bb9a9621302bc0322
sebacaccaro <sebastianocaccaro@gmail.com>
2019-02-08 11:54:01 +0100

    Terminata home

--- php/home.php
+++ php/home.php
@@ -2,12 +2,17 @@
 require_once "Backend/htmlMaker.php";
 require_once "Backend/sql_wrapper.php";
 
-$libri = SqlWrap::query("SELECT Titolo,Autore,Prezzo,ISBN,md5_Hash FROM Libri_In_Vendita ORDER BY Codice_Vendita DESC LIMIT 4");
+$ris = "";
+try{
+      $libri = SqlWrap::query("SELECT Titolo,Autore,Prezzo,ISBN,md5_Hash FROM Libri_In_Vendita ORDER BY Codice_Vendita DESC LIMIT 4");
+      if ($libri)
+            $ris = htmlMaker::generateBookCollection($libri);
+      else
+            $ris = "<p>Sembra che non ci siano libri inseriti nel sito!</p>";
+}catch(Exception $e){
+      $ris = "<p>Sembra che non ci siano libri inseriti nel sito!</p>";
+}
 
-if ($libri)
-    $ris = htmlMaker::generateBookCollection($libri);
-else
-    $ris = "<p>Ti è andata male, non ho trovato niente.</p> <a href='cercalibro.php'>Prova con una nuova ricerca!</a>";
 
 $output = file_get_contents("../HTML/home.html");
 echo str_replace('<a href="home.php">','<a>',
