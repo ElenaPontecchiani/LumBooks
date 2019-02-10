627da85f7791bbd968bf435561c26b05403ec8fc
sebacaccaro <sebastianocaccaro@gmail.com>
2019-02-10 18:34:49 +0100

    indentato js

--- js/action.js
+++ js/action.js
@@ -5,21 +5,19 @@ const RE_NOME = /^[a-zA-Z]{3,16}$/;
 const RE_TEL = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
 const RE_AUTORE = /^[a-zA-Z ]{3,32}$/;
 const RE_CORSO = /^[a-zA-Z0-9 ]{2,64}$/;
-const RE_EDIZIONE= /^[a-zA-Z0-9 ]{3,32}$/;
+const RE_EDIZIONE = /^[a-zA-Z0-9 ]{3,32}$/;
 const RE_ANNO = /^(1|2)[0-9]{3}$/;
 const RE_ISBN = /^[0-9]{13}$/;
 const RE_PREZZO = /(^(\€|\$)?0*[1-9][0-9]{0,3}((,|.)[0-9]{0,2}0*)?$)|(^0*[1-9][0-9]{0,3}((,|.)[0-9]{0,2}0*)?(\€|\$)?$)/;
-window.onload=function()
-{
+window.onload = function () {
 
   // pulsante per l'apertura/chiusura del menu' laterale
-  document.getElementById("openNavButton").addEventListener("click",openNavbar);
-  document.getElementById("close").addEventListener("click",closeNavbar);
+  document.getElementById("openNavButton").addEventListener("click", openNavbar);
+  document.getElementById("close").addEventListener("click", closeNavbar);
 
   // *********** validazione campi Registrazione ******** //
-  if(document.getElementById("registerForm") != null)
-  {
-      items = [
+  if (document.getElementById("registerForm") != null) {
+    items = [
       "email",
       "password",
       "nome",
@@ -28,29 +26,27 @@ window.onload=function()
       "nascita",
       "repeatpassword"
     ];
-    items.forEach(function(item) {
-      document.getElementById(item).addEventListener("focus",checkRegisterInput);
+    items.forEach(function (item) {
+      document.getElementById(item).addEventListener("focus", checkRegisterInput);
     });
 
     //disabilito il form se almeno uno dei seguenti elementi passati come parametro non è corretto
-    disableForm(items,"registerForm",checkRegisterInput);
+    disableForm(items, "registerForm", checkRegisterInput);
   }
 
   // ************ validazione login ********* //
-  if(document.getElementById("loginForm") != null)
-  {
+  if (document.getElementById("loginForm") != null) {
     items = [
       "loginEmail",
       "inputPsw"
     ];
-    items.forEach(function(item) {
-      document.getElementById(item).addEventListener("focus",checkLoginInput);
+    items.forEach(function (item) {
+      document.getElementById(item).addEventListener("focus", checkLoginInput);
     });
-    disableForm(items,"loginForm",checkLoginInput);
+    disableForm(items, "loginForm", checkLoginInput);
   }
-/************ Validazione inserimento libri *******************/
-  if(document.getElementById("insertBox") != null)
-  {
+  /************ Validazione inserimento libri *******************/
+  if (document.getElementById("insertBox") != null) {
     items = [
       "insertEdizione",
       "insertAnno",
@@ -60,17 +56,16 @@ window.onload=function()
       "insertAutore",
       "insertCasaEditrice",
       "insertCorso"
-      ];
+    ];
     document.getElementById("listato").addEventListener("change", changeBooksInput);
     document.getElementById("personale").addEventListener("change", changeBooksInput);
-    items.forEach(function(item) {
-      document.getElementById(item).addEventListener("focus",checkBookInput);
+    items.forEach(function (item) {
+      document.getElementById(item).addEventListener("focus", checkBookInput);
     });
-    disableForm(items,"insertBox",checkBookInput);
+    disableForm(items, "insertBox", checkBookInput);
   }
-/************** Validazione modifica libro *************/
-  if(document.getElementById("modifyBox") != null)
-  {
+  /************** Validazione modifica libro *************/
+  if (document.getElementById("modifyBox") != null) {
     items = [
       "insertEdizione",
       "insertAnno",
@@ -80,29 +75,27 @@ window.onload=function()
       "insertAutore",
       "insertCasaEditrice",
       "insertCorso"
-      ];
-    items.forEach(function(item) {
-      document.getElementById(item).addEventListener("focus",checkBookInput);
+    ];
+    items.forEach(function (item) {
+      document.getElementById(item).addEventListener("focus", checkBookInput);
     });
-    disableForm(items,"modifyBox",checkBookInput);
+    disableForm(items, "modifyBox", checkBookInput);
   }
 
   //nascondere/mostrare le opzioni per il libro listato (inserisci.html)
-  if(document.getElementById('listato') != null)
-  {
+  if (document.getElementById('listato') != null) {
     formselector();
     document.getElementById("listato").addEventListener("change", formselector);
     document.getElementById("personale").addEventListener("change", formselector);
   }
-  
+
   /* mostrare e nascondere regole in regolamento.html */
-  if(document.getElementById("ruleBox") != null)
-  {
+  if (document.getElementById("ruleBox") != null) {
     var input = document.getElementsByTagName("dt");
-    titles = Array.prototype.slice.call(input).map(el=> el.id);
-    titles.forEach(function(el){
+    titles = Array.prototype.slice.call(input).map(el => el.id);
+    titles.forEach(function (el) {
       toggleRuleBox(el);
-      toggleRuleBox(el.replace('Title','Desc'));
+      toggleRuleBox(el.replace('Title', 'Desc'));
     });
   }
 }//end window.onload()
@@ -110,13 +103,12 @@ window.onload=function()
 /*
   attiva o disattiva le descrizioni nel regolamento
 */
-function toggleRuleBox(el){
-  document.getElementById(el).addEventListener("click",function(){
-    descId = el.replace('Title','Desc');
+function toggleRuleBox(el) {
+  document.getElementById(el).addEventListener("click", function () {
+    descId = el.replace('Title', 'Desc');
     document.getElementById(descId).classList.toggle("hidden");
     // non posso usare il last element di css perché dopo di questo c'è un dd che può comparire o scomparire.
-    if(el == "contactsTitle")
-    {
+    if (el == "contactsTitle") {
       document.getElementById(el).classList.toggle("lastElement");
     }
   });
@@ -124,53 +116,45 @@ function toggleRuleBox(el){
 /*
   changeBooksInput cambia i campi obbligatori per "inserisci.html" 
 */
-function changeBooksInput()
-{
+function changeBooksInput() {
   items = [
     "insertTitolo",
     "insertAutore",
     "insertCasaEditrice",
     "insertCorso"
   ];
-  items.forEach(function(item) {
+  items.forEach(function (item) {
     var itemClass = document.getElementById(item).classList;
-    if(itemClass.contains("required"))
-    {
+    if (itemClass.contains("required")) {
       removeErrorBox(document.getElementById(item), true);
       document.getElementById(item).value = "";
       itemClass.remove("required");
-    }else
-    {
+    } else {
       itemClass.add("required");
     }
   });
 }
- 
+
 /*  
   nascondere/mostrare la navbar 
 */
-  function openNavbar()
-  {
-    var nav = document.getElementById("navbar");
-    nav.classList.add("showNavbar");
-  }
+function openNavbar() {
+  var nav = document.getElementById("navbar");
+  nav.classList.add("showNavbar");
+}
 
-  function closeNavbar()
-  {
-    var nav = document.getElementById("navbar");
-    nav.classList.remove("showNavbar");
-  }
-  
+function closeNavbar() {
+  var nav = document.getElementById("navbar");
+  nav.classList.remove("showNavbar");
+}
+
 
 /*
   se una password ripetuta non è corretta segnala un errore
 */
-function checkRepeatPassword(password,rpassword)
-{
-  if(!rpassword.value == '')
-  {
-    if(! (password.value == rpassword.value))
-    {
+function checkRepeatPassword(password, rpassword) {
+  if (!rpassword.value == '') {
+    if (!(password.value == rpassword.value)) {
       setErrorBox(rpassword);
       return false;
     }
@@ -183,10 +167,8 @@ function checkRepeatPassword(password,rpassword)
   se l'item non rispetta l'espressione regolare (RE) => segnala un errore
   altrimenti => rimuovi la segnalazione (se presente)
 */
-function checkItem(item, re)
-{
-  if(!item.value == '' && !re.test(item.value))
-  {
+function checkItem(item, re) {
+  if (!item.value == '' && !re.test(item.value)) {
     setErrorBox(item);
     return false;
   }
@@ -194,29 +176,24 @@ function checkItem(item, re)
   return true;
 }
 
-  /*
-    come checkItem, ma specifico per una data
-  */
-function checkNascita(date)
-{
-  if(!date.value == '')
-  {
+/*
+  come checkItem, ma specifico per una data
+*/
+function checkNascita(date) {
+  if (!date.value == '') {
     correct = true;
     //controllo iniziale, data generica
     var re = /(^(0[1-9]|[12][0-9]|3[01])[\- \/.](?:(0[1-9]|1[012])[\- \/.](19|20)[0-9]{2})$)|(^(19|20)[0-9]{2}[\- \/.](0[1-9]|1[012])[\- \/.](0[1-9]|[12][0-9]|3[01])$)/;
-    if (!re.test(date.value))
-    {
+    if (!re.test(date.value)) {
       correct = false;
     }
     //controllo 31 02,04-06... e febbraio
     re = /^(31.(0[2469]|11).....)$|^(30.02.....)$|^(.....(0[2469]|11).31)$|^(.....02.30)$/;
-    if(re.test(date.value))
-    {
+    if (re.test(date.value)) {
       correct = false;
     }
     //return : true se tutti i controlli passano
-    if(!correct)
-    {
+    if (!correct) {
       setErrorBox(date);
       return false;
     }
@@ -224,10 +201,8 @@ function checkNascita(date)
   removeErrorBox(date);
   return true;
 }
-function setErrorBox(box)
-{
-  if(box.classList.contains('errorBox'))
-  {
+function setErrorBox(box) {
+  if (box.classList.contains('errorBox')) {
     return;
   }
   box.classList.add('errorBox');
@@ -236,73 +211,66 @@ function setErrorBox(box)
   var firstLetter = 'Il';
   var boxName = box.id;
 
-  switch(boxName)
-  {
+  switch (boxName) {
     case 'cel': boxName = 'N. di telefono';
-          break;
-    case 'nascita': boxName = 'data' ;
-          break;
+      break;
+    case 'nascita': boxName = 'data';
+      break;
     case 'inputPsw': boxName = 'password';
-          break;
+      break;
     case 'loginEmail': boxName = 'email';
-          break;
+      break;
     case 'insertTitolo': boxName = 'titolo';
-          break;
+      break;
     case 'insertAutore': boxName = 'autore';
-          break;
+      break;
     case 'insertCasaEditrice': boxName = 'casa editrice';
-          break;
+      break;
     case 'insertCorso': boxName = 'corso';
-          break;
+      break;
     case 'insertEdizione': boxName = 'edizione';
-          break;
+      break;
     case 'insertAnno': boxName = 'anno';
-          break;
+      break;
     case 'insertISBN': boxName = 'isbn';
-          break;
+      break;
     case 'insertPrezzo': boxName = 'prezzo';
   }
   var legend = document.getElementsByTagName("legend")[0];
-  if(boxName == 'password' || boxName == 'email' || boxName == 'nascita' || boxName == 'casa editrice' || boxName == 'edizione' || boxName == 'data')
-  {
-   lastLetter = 'a';
-   firstLetter = 'La';
+  if (boxName == 'password' || boxName == 'email' || boxName == 'nascita' || boxName == 'casa editrice' || boxName == 'edizione' || boxName == 'data') {
+    lastLetter = 'a';
+    firstLetter = 'La';
   }
-  if(boxName == 'isbn' || boxName == 'anno' || boxName == 'email' || boxName == 'edizione')
-  {
+  if (boxName == 'isbn' || boxName == 'anno' || boxName == 'email' || boxName == 'edizione') {
     firstLetter = "L'";
   }
-  legend.outerHTML += (boxName == 'repeatpassword')? 
-    "<p class='errorLine' id='errorMessage"+ box.id +"'>Le password non coincidono.</p>": /* true */
-    "<p class='errorLine' id='errorMessage"+ box.id +"'>"+firstLetter+ " " +boxName +" non è corrett"+ lastLetter +"</p>"; /*false*/
+  legend.outerHTML += (boxName == 'repeatpassword') ?
+    "<p class='errorLine' id='errorMessage" + box.id + "'>Le password non coincidono.</p>" : /* true */
+    "<p class='errorLine' id='errorMessage" + box.id + "'>" + firstLetter + " " + boxName + " non è corrett" + lastLetter + "</p>"; /*false*/
 
 }
 
-function removeErrorBox(box, emptyAllowed = false)
-{
-  if((box.value == "" && emptyAllowed == false) || !box.classList.contains('errorBox'))
-  {
-      return false;
+function removeErrorBox(box, emptyAllowed = false) {
+  if ((box.value == "" && emptyAllowed == false) || !box.classList.contains('errorBox')) {
+    return false;
   }
-  errBox = document.getElementById("errorMessage"+box.id);
-  errBox.outerHTML = ""; 
+  errBox = document.getElementById("errorMessage" + box.id);
+  errBox.outerHTML = "";
   box.classList.remove('errorBox');
   return true;
 }
 
 /* funzione per inserisci.html */
-function formselector()
-{
-    document.getElementById('inserisci-personale').classList.toggle("hidden");
+function formselector() {
+  document.getElementById('inserisci-personale').classList.toggle("hidden");
 }
 
 /* validazione login */
 
-function checkLoginInput()
-{
+function checkLoginInput() {
   var email = document.getElementById("loginEmail");
   var password = document.getElementById("inputPsw");
-  checkItem(password,/^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9!.@#$%^&*]{6,16}$/);
+  checkItem(password, /^(?=.*[0-9])(?=.*[a-z])[a-zA-Z0-9!.@#$%^&*]{6,16}$/);
   checkItem(email, /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 
 }
@@ -310,8 +278,7 @@ function checkLoginInput()
 /*
   /////// validazione campi registrazione ////////
 */
-function checkRegisterInput()
-{
+function checkRegisterInput() {
   var email = document.getElementById("email");
   var password = document.getElementById("password");
   var nome = document.getElementById("nome");
@@ -319,13 +286,13 @@ function checkRegisterInput()
   var cognome = document.getElementById("cognome");
   var nascita = document.getElementById("nascita");
   var repeatpassword = document.getElementById("repeatpassword");
-  checkItem(password,RE_PASSWORD);
+  checkItem(password, RE_PASSWORD);
   checkItem(email, RE_EMAIL);
   checkItem(nome, RE_NOME);
   checkItem(cognome, RE_NOME);
   checkItem(tel, RE_TEL);
   checkNascita(nascita);
-  checkRepeatPassword(password,repeatpassword);
+  checkRepeatPassword(password, repeatpassword);
 }
 
 /*
@@ -334,28 +301,25 @@ function checkRegisterInput()
             form => form da disabilitare
             funzioneValidazione => funzione che valida gli elementi 
 */
-function disableForm(elements, form, funzioneValidazione)
-{
-  if(document.getElementById(form) != null)
-  {
-    elements.forEach(function(el) {
+function disableForm(elements, form, funzioneValidazione) {
+  if (document.getElementById(form) != null) {
+    elements.forEach(function (el) {
       document.getElementById(el).addEventListener("focus", funzioneValidazione);
     });
-    document.getElementById(form).addEventListener("submit", function(event) {
+    document.getElementById(form).addEventListener("submit", function (event) {
       funzioneValidazione();
 
-      if(elements.includes("repeatpassword"))
-        elements.splice( elements.indexOf("repeatpassword"), 1 );
+      if (elements.includes("repeatpassword"))
+        elements.splice(elements.indexOf("repeatpassword"), 1);
 
-      var items = elements.map(function(item) {
+      var items = elements.map(function (item) {
         return document.getElementById(item);
       });
       // controllo campi sono vuoti e segnalazione errore. 
       items.filter(el => (el.classList.contains("required") && el.value == ""))
         .forEach(element => setErrorBox(element));
       errors = document.getElementsByClassName("errorLine");
-      if (!errors.length==0)
-      {
+      if (!errors.length == 0) {
         event.preventDefault();
       }
     });
@@ -364,8 +328,7 @@ function disableForm(elements, form, funzioneValidazione)
 /*
   Validazione dei campi inserimento libro
 */
-function checkBookInput()
-{
+function checkBookInput() {
   var autore = document.getElementById("insertAutore");
   var corso = document.getElementById("insertCorso");
   var edizione = document.getElementById("insertEdizione");
