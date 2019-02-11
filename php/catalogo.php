b54f4e3954d4dd73b363dfaaf283a756a3984c3a
stefano <stefano.sgt39@gmail.com>
2019-02-11 13:33:47 +0100

    fix catalogo

--- php/catalogo.php
+++ php/catalogo.php
@@ -6,7 +6,7 @@ $query = "  SELECT Titolo,Autore,Casa_Editrice as Editore, Corso
             FROM Libri_Listati";
 $libri = htmlMaker::generateBookCollection(SqlWrap::query($query));
 $output = file_get_contents("../HTML/catalogo.html");
-$output = str_replace("<ul class='books_colelction'></ul>",$libri,$output);
+$output = str_replace("<ul class='books_collection'></ul>",$libri,$output);
 $output = str_replace("<header></header>",htmlMaker::header(),$output);
 $output = str_replace("<nav></nav>",      htmlMaker::navbar(),$output);
 $output = str_replace('<a href="catalogo.php">',"<a>", $output);
